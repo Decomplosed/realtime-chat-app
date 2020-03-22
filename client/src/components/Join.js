2e604e6200f5710c24638355a1f9666f81adb790
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-22 22:42:19 +0100

    useState for name and room

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Join = () => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
   return <h1>Join</h1>
 }
 
