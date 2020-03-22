e9e02531bb718d6e3c244df2c413ee6fc53a71ce
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-22 22:18:58 +0100

    set Router inside App

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const App = () => (
+  <Router>
+    <Route exact path='/' component={Join} />
+    <Route path='/chat' component={Chat} />
+  </Router>
+)
