1292602ce7614cf70faa46a2a387ca4d569bfea0
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-23 14:21:39 +0100

    declared empty socket variable

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
+let socket
+
 const Chat = ({ location }) => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
