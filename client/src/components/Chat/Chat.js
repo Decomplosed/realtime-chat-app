4b75b8c204062110b359c054332943dade607859
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-23 15:40:19 +0100

    wrote a basic template for Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -36,7 +36,29 @@ const Chat = ({ location }) => {
     })
   }, [messages])
 
-  return <h1>Chat</h1>
+  const sendMessage = event => {
+    event.preventDefault()
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
+  console.log(message, messages)
+
+  return (
+    <div className='outerContainer'>
+      <div className='container'>
+        <input
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event =>
+            event.key === 'Enter' ? sendMessage(event) : null
+          }
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Chat
