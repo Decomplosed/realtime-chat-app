37f5685d274e22b86927b8573f0d269aceeb9f25
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-23 15:18:23 +0100

    finished writing getUsersInRoom function

--- server/users.js
+++ server/users.js
@@ -25,6 +25,6 @@ const removeUser = id => {
   }
 }
 
-const getUser = (id) => users.find(user => user.id === id)
+const getUser = id => users.find(user => user.id === id)
 
-const getUsersInRoom = () => {}
+const getUsersInRoom = room => users.filter(user => user.room === room)
