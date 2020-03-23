b3bbc0a4fb35b7d90a13b22a4fb34ecef4ac688f
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-23 14:20:34 +0100

    store name and room from location.search using queryString

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,16 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search)
+
+    console.log(name, room)
+  })
+
   return <h1>Chat</h1>
 }
 
