fff52439f5a18f63e0179dc3844f7a3b9868132f
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-24 11:47:29 +0100

    created template for InfoBar component

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 import './InfoBar.css'
 
-function InfoBar() {
-  return <div></div>
-}
+const InfoBar = () => (
+  <div className='infoBar'>
+    <div className='leftInnerContainer'>
+      <img className='onlineIcon' src={onlineIcon} alt='online image' />
+      <h3>{/*roomName*/}</h3>
+    </div>
+    <div className='rightInnerContainer'>
+      <a href='/'>
+        <img src={closeIcon} alt='close image' />
+      </a>
+    </div>
+  </div>
+)
 
 export default InfoBar
